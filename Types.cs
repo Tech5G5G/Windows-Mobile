499ee4f51631714c78fdc183fe838bbbd9585559
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-11-12 22:11:01 -0800

    Added property image to MCModInfo

--- Types.cs
+++ Types.cs
@@ -52,10 +52,11 @@ public class MCModInfo
     public string name { get; set; }
     public string version { get; set; }
     public string description { get; set; }
-    public string icon { get; set; }
     public string license { get; set; }
     public MCModContact contact { get; set; }
 
+    /// <summary>Path to icon</summary>
+    public string icon { get; set; }
     public BitmapImage image { get; set; }
 }
 
