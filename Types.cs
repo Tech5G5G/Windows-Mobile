5a6c7424e178ad41aa1d9c4606cf2af1cebc7520
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-11-13 15:19:41 -0800

    Added MC mod kind

--- Types.cs
+++ Types.cs
@@ -54,6 +54,7 @@ public class MCModInfo
     public string description { get; set; }
     public string license { get; set; }
     public MCModContact contact { get; set; }
+    public ModKind kind { get; set; }
 
     /// <summary>Path to icon</summary>
     public string icon { get; set; }
@@ -65,4 +66,12 @@ public class MCModContact
     public string homepage { get; set;}
     public string sources { get; set;}
     public string issues { get; set;}
+}
+
+///<summary>Used for identifying mods</summary>
+public enum ModKind
+{
+    FabricQuilt,
+    Forge,
+    NeoForge
 }
