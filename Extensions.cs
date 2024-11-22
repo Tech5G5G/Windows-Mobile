6498b014168a6a2434bbe9730c4cf2a3418e76d5
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-11-22 15:52:44 -0800

    ToMonthName extension

--- Extensions.cs
+++ Extensions.cs
@@ -27,5 +27,28 @@ namespace Windows_Mobile
             else
                 throw new Exception("Unable to compare value of input");
         }
+
+        /// <summary>Converts a number from 1 to 12 into a month name</summary>
+        /// <returns>The name of the month</returns>
+        /// <exception cref="InvalidDataException"/>
+        public static string ToMonthName(this int monthNum)
+        {
+            return monthNum switch
+            {
+                1 => "January",
+                2 => "February",
+                3 => "March",
+                4 => "April",
+                5 => "May",
+                6 => "June",
+                7 => "July",
+                8 => "August",
+                9 => "September",
+                10 => "October",
+                11 => "November",
+                12 => "December",
+                _ => throw new InvalidDataException("Value is not a a month number (1, 7, 12, etc.)"),
+            };
+        }
     }
 }
